2961cc88af0e283b2be0e3b5098b617283ec2d70
eyujunior <eyujunior052@gmail.com>
2020-10-27 14:41:20 -0700

    the whole challenge done at once

---
+++ app.js
@@ -0,0 +1,10 @@
+const checkbox = document.querySelector('input');
+const header = document.querySelector('.header');
+const main = document.querySelector('.main');
+
+const lightTheme = () => {
+    header.classList.toggle('light')
+    main.classList.toggle('white')
+}
+
+
